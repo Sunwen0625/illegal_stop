8b04ed0ce01189e822577a28787b8f1c0ac159df
yeeyeeeyee <aagg50211@gmail.com>
2024-12-15 15:46:13 +0800

    refactor : 讓他讀取mqtt就好

--- mqtt/main.py
+++ mqtt/main.py
@@ -3,13 +3,13 @@ import json
 
 
 # 當地端程式連線伺服器得到回應時，要做的動作
-def on_connect(client, userdata, flags, rc):
+def on_connect(client:mqtt.Client, userdata, flags, rc,properties=None):
     print("Connected with result code "+str(rc))
     # 訂閱主題
-    client.subscribe("sunwen_gps")
+    client.subscribe("sunwen_gps",qos=2)
 
 # 當接收到從伺服器發送的訊息時要進行的動作
-def on_message(client, userdata, msg):
+def on_message(client:mqtt.Client, userdata, msg:mqtt.MQTTMessage):
     message = msg.payload.decode('utf-8')
     print(f"收到的訊息: {msg.topic} - {message}")
     try:
@@ -20,30 +20,23 @@ def on_message(client, userdata, msg):
     except json.JSONDecodeError as e:
         print(f"無法解析 JSON: {e}")
 
-# 發佈訊息的方法
-def publish_message(client):
-    topic = "sunwen_get"
-    while True:
-        # 從使用者輸入訊息
-        message = input("請輸入要傳輸的訊息 (輸入 'exit' 離開): ")
-        if message.lower() == "exit":
-            print("已結束發佈訊息。")
-            break
-        client.publish(topic, message)
-        print(f"已發佈訊息到主題 '{topic}': {message}")
 
-# MQTT 客戶端執行緒
-def mqtt_thread():
-    client = mqtt.Client()
+def init_mqtt() -> mqtt.Client:
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2,protocol=mqtt.MQTTv5, )
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect("broker.emqx.io", 1883, 60)
+    return client
+
+# MQTT 客戶端執行緒
+def mqtt_thread():
+    client = init_mqtt()
 
     # 啟動 MQTT 迴圈
-    client.loop_start()
-    publish_message(client)
-    client.loop_stop()
-    client.disconnect()
+    client.loop_forever()
 
+
+if __name__ == '__main__':
+    mqtt_thread()
 
 
